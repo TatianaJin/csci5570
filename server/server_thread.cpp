12d27285452f5d840fc6128c312166e55339d2d4
tkwong <tkwong@railsant.com>
2017-09-27 00:56:20 +0800

    Implemented ServerThread::RegisterModel and ServerThread::GetModel

--- server/server_thread.cpp
+++ server/server_thread.cpp
@@ -5,9 +5,16 @@
 namespace csci5570 {
 
 void ServerThread::RegisterModel(uint32_t model_id, std::unique_ptr<AbstractModel>&& model) {
+  models_.insert(std::make_pair(model_id, std::move(model)));
 }
 
 AbstractModel* ServerThread::GetModel(uint32_t model_id) {
+  auto it = models_.find(model_id);
+  if (it != models_.end()){
+     return it->second.get();
+   } else {
+     return nullptr;
+   }
 }
 
 void ServerThread::Main() {
