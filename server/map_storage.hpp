8177dd0fe2942c990415d7186a8603461af9385d
lenny098 <32307017+lenny098@users.noreply.github.com>
2017-09-27 08:43:21 +0800

    Implemented SubAdd and SubGet functions

--- server/map_storage.hpp
+++ server/map_storage.hpp
@@ -19,11 +19,23 @@ class MapStorage : public AbstractStorage {
     auto typed_vals = third_party::SArray<Val>(vals);
     CHECK_EQ(typed_keys.size(), typed_vals.size());
     // TODO
+    for (int i=0; i<typed_keys.size(); i++)
+    {
+       	storage_.insert(std::make_pair(typed_keys[i], typed_vals[i]));
+    }
   }
 
   virtual third_party::SArray<char> SubGet(const third_party::SArray<Key>& typed_keys) override {
     third_party::SArray<Val> reply_vals(typed_keys.size());
     // TODO
+    for (int i=0; i<typed_keys.size(); i++)
+    {
+        auto it = storage_.find(typed_keys[i]);
+    	   if (it != storage_.end())
+    		      reply_vals[i] = it->second;
+    	   else
+    		      break; //If a key does not exist, return the values founded so far.
+    }
     return third_party::SArray<char>(reply_vals);
   }
 
