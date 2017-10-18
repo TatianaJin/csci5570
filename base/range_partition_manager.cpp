2c5ab9189a77bc867b0fbc1641848f89b4f10ca4
huangyunmu <andyhuangyunmu@gmail.com>
2017-10-18 13:56:36 +0800

    Add Renge parition manager push 1

---
+++ base/range_partition_manager.cpp
@@ -0,0 +1,58 @@
+#pragma once
+#include "base/range_partition_manager.cpp"
+
+namespace csci5570 {
+
+	/*class RangePartitionManager : public AbstractPartitionManager {
+	public:
+		RangePartitionManager(const std::vector<uint32_t>& server_thread_ids, const std::vector<third_party::Range>& ranges)
+			: AbstractPartitionManager(server_thread_ids) {}
+
+		void Slice(const Keys& keys, std::vector<std::pair<int, Keys>>* sliced) override {}
+
+		void Slice(const KVPairs& kvs, std::vector<std::pair<int, KVPairs>>* sliced) override {}
+		private:
+		std::vector<third_party::Range> ranges_;
+	};*/
+	
+
+	RangePartitionManager::RangePartitionManager(const std::vector<uint32_t>& server_thread_ids, const std::vector<third_party::Range>& ranges) {
+		this.ranges_ = ranges;
+
+	}
+	void RangePartitionManager::Slice(const Keys& keys, std::vector<std::pair<int, Keys>>* sliced) {
+		const int keys_size = keys.size();//Num of keys
+		const int servers_size = GetNumServers();//Num of Server
+		int kvs_per_server = keysCount / servers_size;	//keys per server
+														//Range slice
+														//Keys on first server to n-1 server
+		pair<int, keys> tempPair;
+		for (int i = 0; i < serverCount - 1; i++) {
+			tempPair = { GetServerThreadIds[i] ,keys.segment(i*keys_per_server,(i + 1)*keys_per_server };
+			sliced->push_back(tempPair);
+		}
+		//Keys on the last server
+		tempPair = { GetServerThreadIds[servers_size - 1],keys.segment((servers_size - 1)*kvs_per_server,keys.size()) };
+		sliced->push_back(tempPair);
+
+	}
+	void RangePartitionManager::Slice(const KVPairs& kvs, std::vector<std::pair<int, KVPairs>>* sliced) {
+		EXPECT_EQ(kvs.first.size(), kvs.second.size());
+		const int kvs_size = kvs.size();//Num of keys
+		const int servers_size = GetNumServers();//Num of Server
+		int kvs_per_server = keysCount / servers_size;	//keys per server
+														//Range slice
+														//Keys on first server to n-1 server
+		pair<int, keys> tempPair;
+		for (int i = 0; i < serverCount - 1; i++) {
+			tempPair = { GetServerThreadIds[i] ,kvs.segment(i*keys_per_server,(i + 1)*keys_per_server };
+			sliced->push_back(tempPair);
+		}
+		//Keys on the last server
+		tempPair = { GetServerThreadIds[servers_size - 1],kvs.segment((servers_size - 1)*kvs_per_server,keys.size()) };
+		sliced->push_back(tempPair);
+
+	}
+
+
+}  // namespace csci5570
--- server/map_storage.hpp
+++ server/map_storage.hpp
@@ -9,35 +9,34 @@
 
 namespace csci5570 {
 
-template <typename Val>
-class MapStorage : public AbstractStorage {
- public:
-  MapStorage() = default;
+	template <typename Val>
+	class MapStorage : public AbstractStorage {
+	public:
+		MapStorage() = default;
 
-  virtual void SubAdd(const third_party::SArray<Key>& typed_keys,
-      const third_party::SArray<char>& vals) override {
-    auto typed_vals = third_party::SArray<Val>(vals);
-    CHECK_EQ(typed_keys.size(), typed_vals.size());
-    // TODO
-    for(size_t index=0;index<typed_keys.size();index++){
-            storage_[typed_keys[index]]+=typed_vals[index];
-    }
-    LOG(INFO) << "Test by Andy";
-  }
+		virtual void SubAdd(const third_party::SArray<Key>& typed_keys,
+			const third_party::SArray<char>& vals) override {
+			auto typed_vals = third_party::SArray<Val>(vals);
+			CHECK_EQ(typed_keys.size(), typed_vals.size());
+			// TODO
+			for (size_t index = 0; index < typed_keys.size(); index++) {
+				storage_[typed_keys[index]] += typed_vals[index];
+			}
+		}
 
-  virtual third_party::SArray<char> SubGet(const third_party::SArray<Key>& typed_keys) override {
-    third_party::SArray<Val> reply_vals(typed_keys.size());
-    // TODO
-            for(int i=0;i<typed_keys.size();i++){
-                    reply_vals[i]=storage_[typed_keys[i]];
-            }
-    return third_party::SArray<char>(reply_vals);
-  }
+		virtual third_party::SArray<char> SubGet(const third_party::SArray<Key>& typed_keys) override {
+			third_party::SArray<Val> reply_vals(typed_keys.size());
+			// TODO
+			for (int i = 0; i < typed_keys.size(); i++) {
+				reply_vals[i] = storage_[typed_keys[i]];
+			}
+			return third_party::SArray<char>(reply_vals);
+		}
 
-  virtual void FinishIter() override {}
+		virtual void FinishIter() override {}
 
- private:
-  std::map<Key, Val> storage_;
-};
+	private:
+		std::map<Key, Val> storage_;
+	};
 
 }  // namespace csci5570
