aa08d4fb894fded748a9178d1a7048763128d0d8
huangyunmu <andyhuangyunmu@gmail.com>
2017-10-19 20:47:29 +0800

    update abstract hpp

--- base/abstract_partition_manager.hpp
+++ base/abstract_partition_manager.hpp
@@ -8,28 +8,28 @@
 
 namespace csci5570 {
 
-/*
- * Implments the interface of a PartitionManager which provides the model partitioning scheme
- */
-class AbstractPartitionManager {
- public:
-  // using Keys = std::vector<Key>;
-  // using KVPairs = std::pair<std::vector<Key>, std::vector<double>>;
-  using Keys = third_party::SArray<Key>;
-  using KVPairs = std::pair<third_party::SArray<Key>, third_party::SArray<double>>;
+	/*
+	* Implments the interface of a PartitionManager which provides the model partitioning scheme
+	*/
+	class AbstractPartitionManager {
+	public:
+		// using Keys = std::vector<Key>;
+		// using KVPairs = std::pair<std::vector<Key>, std::vector<double>>;
+		using Keys = third_party::SArray<Key>;
+		using KVPairs = std::pair<third_party::SArray<Key>, third_party::SArray<double>>;
 
-  AbstractPartitionManager(const std::vector<uint32_t>& server_thread_ids) : server_thread_ids_(server_thread_ids) {}
+		AbstractPartitionManager(const std::vector<uint32_t>& server_thread_ids) : server_thread_ids_(server_thread_ids) {}
 
-  size_t GetNumServers() const;
-  const std::vector<uint32_t>& GetServerThreadIds() const;
+		size_t GetNumServers() const;
+		const std::vector<uint32_t>& GetServerThreadIds() const;
 
-  // slice keys into <server_id, key_partition> pairs
-  virtual void Slice(const Keys& keys, std::vector<std::pair<int, Keys>>* sliced) = 0;
-  // slice key-value pairs into <server_id, key_value_partition> pairs
-  virtual void Slice(const KVPairs& kvs, std::vector<std::pair<int, KVPairs>>* sliced) = 0;
+		// slice keys into <server_id, key_partition> pairs
+		virtual void Slice(const Keys& keys, std::vector<std::pair<int, Keys>>* sliced) const = 0;
+		// slice key-value pairs into <server_id, key_value_partition> pairs
+		virtual void Slice(const KVPairs& kvs, std::vector<std::pair<int, KVPairs>>* sliced) const = 0;
 
- protected:
-  std::vector<uint32_t> server_thread_ids_;
-};  // class AbstractPartitionManager
+	protected:
+		std::vector<uint32_t> server_thread_ids_;
+	};  // class AbstractPartitionManager
 
 }  // namespace csci5570
