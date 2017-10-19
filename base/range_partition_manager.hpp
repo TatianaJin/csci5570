0a3f6a1909ed0d692d243243ad3c62ff048a43a3
huangyunmu <andyhuangyunmu@gmail.com>
2017-10-19 17:50:07 +0800

    Change  hpp

--- base/range_partition_manager.hpp
+++ base/range_partition_manager.hpp
@@ -11,17 +11,17 @@
 
 namespace csci5570 {
 
-class RangePartitionManager : public AbstractPartitionManager {
- public:
-  RangePartitionManager(const std::vector<uint32_t>& server_thread_ids, const std::vector<third_party::Range>& ranges)
-      : AbstractPartitionManager(server_thread_ids) {}
+	class RangePartitionManager : public AbstractPartitionManager {
+	public:
+		RangePartitionManager(const std::vector<uint32_t>& server_thread_ids, const std::vector<third_party::Range>& ranges)
+			: AbstractPartitionManager(server_thread_ids) {}
 
-  void Slice(const Keys& keys, std::vector<std::pair<int, Keys>>* sliced) const override {}
+		void Slice(const Keys& keys, std::vector<std::pair<int, Keys>>* sliced) const override {}
 
-  void Slice(const KVPairs& kvs, std::vector<std::pair<int, KVPairs>>* sliced) const override {}
+		void Slice(const KVPairs& kvs, std::vector<std::pair<int, KVPairs>>* sliced) const override {}
 
- private:
-  std::vector<third_party::Range> ranges_;
-};
+	private:
+		std::vector<third_party::Range> ranges_;
+	};
 
 }  // namespace csci5570
