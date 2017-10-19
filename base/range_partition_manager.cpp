c1de70047805e7920f08c9bea40501bee2eeedb6
huangyunmu <andyhuangyunmu@gmail.com>
2017-10-19 21:08:54 +0800

    bug 3

--- base/range_partition_manager.cpp
+++ base/range_partition_manager.cpp
@@ -14,7 +14,7 @@ namespace csci5570 {
 		private:
 		std::vector<third_party::Range> ranges_;
 	};*/
-	
+
 
 	RangePartitionManager::RangePartitionManager(const std::vector<uint32_t>& server_thread_ids, const std::vector<third_party::Range>& ranges) {
 		this.ranges_ = ranges;
@@ -24,16 +24,16 @@ namespace csci5570 {
 		LOG(INFO) << "Test by Andy";
 		const int keys_size = keys.size();//Num of keys
 		const int servers_size = GetNumServers();//Num of Server
-		int kvs_per_server = keysCount / servers_size;	//keys per server
-														//Range slice
-														//Keys on first server to n-1 server
+		int keys_per_server = keys_size / servers_size;	//keys per server
+														//Range slice			
 		pair<int, keys> tempPair;
+		//Keys on first server to n-1 server
 		for (int i = 0; i < serverCount - 1; i++) {
 			tempPair = { GetServerThreadIds[i] ,keys.segment(i*keys_per_server,(i + 1)*keys_per_server };
 			sliced->push_back(tempPair);
 		}
 		//Keys on the last server
-		tempPair = { GetServerThreadIds[servers_size - 1],keys.segment((servers_size - 1)*kvs_per_server,keys.size()) };
+		tempPair = { GetServerThreadIds[servers_size - 1],keys.segment((servers_size - 1)*keys_per_server,keys.size()) };
 		sliced->push_back(tempPair);
 
 	}
@@ -41,12 +41,12 @@ namespace csci5570 {
 		EXPECT_EQ(kvs.first.size(), kvs.second.size());
 		const int kvs_size = kvs.size();//Num of keys
 		const int servers_size = GetNumServers();//Num of Server
-		int kvs_per_server = keysCount / servers_size;	//keys per server
+		int kvs_per_server = kvs_size / servers_size;	//keys per server
 														//Range slice
 														//Keys on first server to n-1 server
 		pair<int, keys> tempPair;
 		for (int i = 0; i < serverCount - 1; i++) {
-			tempPair = { GetServerThreadIds[i] ,kvs.segment(i*keys_per_server,(i + 1)*keys_per_server };
+			tempPair = { GetServerThreadIds[i] ,kvs.segment(i*kvs_per_server,(i + 1)*kvs_per_server };
 			sliced->push_back(tempPair);
 		}
 		//Keys on the last server
