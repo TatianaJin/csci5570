74f1df5954563f041dd0b092f000f5a26fde717a
huangyunmu <andyhuangyunmu@gmail.com>
2017-10-20 13:41:41 +0800

    Bug Fix 1341

--- base/range_partition_manager.cpp
+++ base/range_partition_manager.cpp
@@ -22,8 +22,7 @@ namespace csci5570 {
 	}
 
 	void RangePartitionManager::Slice(const Keys& keys, std::vector<std::pair<int, Keys>>* sliced) const {
-		LOG(INFO) << "Test by Andy";
-
+		LOG(INFO) << "Test by Andy 1";
 		const int keys_size = keys.size();//Num of keys
 		const int range_size = this->ranges_.size();
 
@@ -43,38 +42,27 @@ namespace csci5570 {
 		}
 
 	}
-	//void RangePartitionManager::Slice(const Keys& keys, std::vector<std::pair<int, Keys>>* sliced) const  {
-	//	LOG(INFO) << "Test by Andy";
-	//	const int keys_size = keys.size();//Num of keys
-	//	const int servers_size = this->GetNumServers();//Num of Server
-	//	int keys_per_server = keys_size / servers_size;	//keys per server
-	//													//Range slice			
-	//	std::pair<int, Keys> tempPair;
-	//	//Keys on first server to n-1 server
-	//	for (int i = 0; i < servers_size - 1; i++) {
-	//		tempPair = { (this->GetServerThreadIds())[i] ,keys.segment(i*keys_per_server,(i + 1)*keys_per_server) };
-	//		sliced->push_back(tempPair);
-	//	}
-	//	//Keys on the last server
-	//	tempPair = { (this->GetServerThreadIds())[servers_size - 1],keys.segment((servers_size - 1)*keys_per_server,keys.size()) };
-	//	sliced->push_back(tempPair);
 
-	//}
 	void RangePartitionManager::Slice(const KVPairs& kvs, std::vector<std::pair<int, KVPairs>>* sliced)const  {
-		//const int kvs_size = kvs.size();//Num of keys
-		//const int servers_size = GetNumServers();//Num of Server
-		//int kvs_per_server = kvs_size / servers_size;	//keys per server
-		//												//Range slice
-		//												//Keys on first server to n-1 server
-		//std::pair<int, keys> tempPair;
-		//for (int i = 0; i < serverCount - 1; i++) {
-		//	tempPair = { GetServerThreadIds[i] ,kvs.segment(i*kvs_per_server,(i + 1)*kvs_per_server };
-		//	sliced->push_back(tempPair);
-		//}
-		////Keys on the last server
-		//tempPair = { GetServerThreadIds[servers_size - 1],kvs.segment((servers_size - 1)*kvs_per_server,keys.size()) };
-		//sliced->push_back(tempPair);
+		LOG(INFO) << "Test by Andy 2";
+		Keys keys = kvs.first;
+		const int keys_size = keys.size();//Num of keys
+		const int range_size = this->ranges_.size();
 
+		for (int j = 0; j < range_size; j++)
+		{
+			KVPairs tempKVs;
+			for (int i = 0; i < keys_size; i++) {
+				if (keys[i] >= this->ranges_[j].begin() && keys[i] < this->ranges_[j].end()) {
+					tempKVs.push_back(kvs[i]);
+				}
+			}
+			if (tempKeys.size() > 0) {
+				std::pair<int, tempKVs> tempPair(this->server_thread_ids_[j], tempKVs);
+				sliced->push_back(tempPair);
+			}
+
+		}
 	}
 
 
